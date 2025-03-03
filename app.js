3e294d189506005d607e0f7ea5c2751977b6e4ce
mohitjoer <mohitjoe.r@gmail.com>
2025-03-03 15:09:26 +0530

    commit	modified:   app.js commit	modified:   index.html commit	new file:   pngs/moon.svg commit	new file:   pngs/sunny.svg commit	modified:   style.css

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let theme = document.querySelector('#mode');
+theme.addEventListener('click', () => {
+  console.log('clicked');
+  this.document.innerhtml = "Hello";
+});
+
+
 
 var typed = new Typed('.text', {
     strings: ["Frontend Developer","Youtuber","Web Developer"],
